ca8076a916a41db38b14f4549d9193f02c78b7c1
ozkanonur <work@onurozkan.dev>
2022-02-28 18:35:56 +0300

    create new package `io`
    Signed-off-by: ozkanonur <work@onurozkan.dev>

---
+++ io/src/file.rs
@@ -0,0 +1,28 @@
+use std::{
+    fs::{self, create_dir_all},
+    path::Path,
+};
+
+pub fn copy_recursively(src: &str, destination: &str) -> Result<(), std::io::Error> {
+    create_dir_all(destination.clone())?;
+
+    let src = Path::new(src);
+    let destination = Path::new(destination);
+
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+        if file_type.is_dir() {
+            copy_recursively(
+                entry.path().to_str().unwrap(),
+                destination.join(entry.file_name()).to_str().unwrap(),
+            )?;
+        } else {
+            fs::copy(
+                entry.path(),
+                destination.join(entry.file_name()).to_str().unwrap(),
+            )?;
+        }
+    }
+    Ok(())
+}
