f19aabb512be2c85218784c8a7f5e25cc71186c5
Onur Özkan <work@onurozkan.dev>
2022-04-02 16:13:09 +0300

    define lpm versioning values
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,6 +4,10 @@ use std::{error, io};
 
 const EXTRACTION_OUTPUT_PATH: &str = "/var/cache/lpm";
 
+const _V_MAJOR: &i8 = 0;
+const _V_MINOR: &i8 = 0;
+const _V_PATCH: &i8 = 0;
+
 pub mod extraction;
 pub mod installation;
 pub mod pkg;
