ab02e324f5bb7757fcb5ecccfca5428112e4e1b6
Onur Özkan <work@onurozkan.dev>
2022-07-29 14:18:01 +0300

    progress poc
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,61 +1,26 @@
-use common::pkg::LodPkg;
-use core::{deletion::DeletionTasks, installation::InstallationTasks};
-use db::init_db;
-use db::pkg::LodPkgCoreDbOps;
-use db::{pkg::delete_pkg_kinds, pkg::insert_pkg_kinds, DB_PATH};
-use min_sqlite3_sys::prelude::*;
-use std::env;
-use std::path::Path;
+use std::{
+    io::{self, Write},
+    thread::sleep,
+    time::Duration,
+};
 
-#[allow(unused_imports)]
-use ehandle::{RuntimeError, RuntimeErrorKind};
+fn progress() -> io::Result<()> {
+    let mut stdout = io::stdout();
+    let mut handle = stdout.lock();
 
-#[cfg(target_os = "linux")]
-fn main() -> Result<(), RuntimeError> {
-    init_db()?;
+    for val in 0..=100 {
+        handle.write_all(format!("\r{}", val).as_bytes())?;
+        stdout.flush()?;
 
-    let args: Vec<String> = env::args().collect();
-
-    let cli = |arg: &str| -> Result<(), RuntimeError> {
-        match arg {
-            "--install" => {
-                let mut pkg = LodPkg::from_fs(args.get(2).expect("Package path is missing."));
-                pkg.start_installation()?;
-            }
-            "--delete" => {
-                let db = Database::open(Path::new(DB_PATH)).unwrap();
-                let pkg = LodPkg::from_db(&db, args.get(2).expect("Package name is missing."))?;
-                db.close();
-
-                pkg.start_deletion()?;
-            }
-            "--add-pkg-kind" => {
-                let db = Database::open(Path::new(DB_PATH))?;
-                let kinds = &args[2..];
-                insert_pkg_kinds(&db, kinds.to_vec())?;
-                db.close();
-            }
-            "--delete-pkg-kind" => {
-                let db = Database::open(Path::new(DB_PATH))?;
-                let kinds = &args[2..];
-                delete_pkg_kinds(&db, kinds.to_vec())?;
-                db.close();
-            }
-            _ => panic!("Invalid argument."),
-        };
-
-        Ok(())
-    };
-
-    match args.get(1) {
-        Some(arg) => cli(arg)?,
-        None => panic!("Missing argument"),
+        sleep(Duration::from_millis(61));
     }
+
+    // \n
+    handle.write_all(&[10])?;
 
     Ok(())
 }
 
-#[cfg(not(target_os = "linux"))]
-fn main() -> Result<(), RuntimeError> {
-    Err(RuntimeErrorKind::UnsupportedPlatform(None).throw())
+fn main() -> io::Result<()> {
+    progress()
 }
