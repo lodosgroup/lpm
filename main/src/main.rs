cf1b80e68a2dbeaf06a31d5d9543806e6ab3ee54
ozkanonur <work@onurozkan.dev>
2022-07-30 23:21:02 +0300

    save development state
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,8 +1,43 @@
 use std::{
     io::{self, Write},
+    sync::Arc,
     thread::sleep,
     time::Duration,
 };
+
+struct ProgressBar<'a> {
+    init_message: &'a str,
+    final_message: &'a str,
+    states: Vec<Arc<ProgressState>>,
+    status: Arc<bool>,
+}
+
+struct ProgressState {
+    state: u8,
+    max_val: u8,
+}
+
+impl<'a> ProgressBar<'a> {
+    fn new(init_message: &'a str, final_message: &'a str) -> Self {
+        Self {
+            init_message,
+            final_message,
+            states: todo!(),
+            status: todo!(),
+        }
+    }
+
+    fn add_bar(&mut self) -> Arc<ProgressState> {
+        let state = Arc::new(ProgressState {
+            state: 0,
+            max_val: 100,
+        });
+
+        self.states.push(state.clone());
+
+        state
+    }
+}
 
 fn progress() -> io::Result<()> {
     let mut stdout = io::stdout();
@@ -24,3 +59,7 @@ fn progress() -> io::Result<()> {
 fn main() -> io::Result<()> {
     progress()
 }
+
+// TODO
+// Keep printing the duration
+// and let the progress state trigger manually
