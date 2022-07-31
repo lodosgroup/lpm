969a4e74725203a7f02b9bd6911c641459250470
ozkanonur <work@onurozkan.dev>
2022-07-31 22:37:50 +0300

    save development state
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,15 +1,15 @@
 use std::{
     io::{self, Write},
-    sync::Arc,
-    thread::sleep,
+    sync::{Arc, Mutex, RwLock},
+    thread,
     time::Duration,
 };
 
 struct ProgressBar<'a> {
     init_message: &'a str,
     final_message: &'a str,
-    states: Vec<Arc<ProgressState>>,
-    status: Arc<bool>,
+    states: Vec<Arc<RwLock<ProgressState>>>,
+    status: bool,
 }
 
 struct ProgressState {
@@ -27,11 +27,11 @@ impl<'a> ProgressBar<'a> {
         }
     }
 
-    fn add_bar(&mut self) -> Arc<ProgressState> {
-        let state = Arc::new(ProgressState {
+    fn add_bar(&mut self) -> Arc<RwLock<ProgressState>> {
+        let state = Arc::new(RwLock::new(ProgressState {
             state: 0,
             max_val: 100,
-        });
+        }));
 
         self.states.push(state.clone());
 
@@ -39,25 +39,74 @@ impl<'a> ProgressBar<'a> {
     }
 }
 
-fn progress() -> io::Result<()> {
-    let mut stdout = io::stdout();
-    let mut handle = stdout.lock();
+impl ProgressState {
+    fn start(&self) -> io::Result<()> {
+        let mut stdout = io::stdout();
+        let mut handle = stdout.lock();
 
-    for val in 0..=100 {
-        handle.write_all(format!("\r{}", val).as_bytes())?;
-        stdout.flush()?;
+        while self.state != self.max_val {
+            handle.write_all(format!("\r{}", self.state).as_bytes())?;
+            stdout.flush()?;
 
-        sleep(Duration::from_millis(61));
+            thread::sleep(Duration::from_millis(61));
+        }
+
+        // \n
+        handle.write_all(&[10])?;
+
+        Ok(())
     }
 
-    // \n
-    handle.write_all(&[10])?;
+    fn increment(&mut self, by: u8) {
+        self.state += by;
+    }
 
-    Ok(())
+    fn finish(&mut self, by: u8) {
+        self.state = self.max_val;
+    }
 }
 
+//fn progress() -> io::Result<()> {
+//    let mut stdout = io::stdout();
+//    let mut handle = stdout.lock();
+//
+//    for val in 0..=100 {
+//        handle.write_all(format!("\r{}", val).as_bytes())?;
+//        stdout.flush()?;
+//
+//        sleep(Duration::from_millis(61));
+//    }
+//
+//    // \n
+//    handle.write_all(&[10])?;
+//
+//    Ok(())
+//}
+//
+//fn main() -> io::Result<()> {
+//    progress()
+//}
+
 fn main() -> io::Result<()> {
-    progress()
+    let mut pbar = ProgressBar::new("Starting", "Finished");
+
+    let mut pstate = pbar.add_bar();
+
+    let pstate_thread = pstate.clone();
+    let pthread = thread::spawn(move || {
+        let x = pstate_thread.read().unwrap();
+        x.start().unwrap();
+    });
+
+    loop {
+        thread::sleep(Duration::from_millis(61));
+        let mut x = pstate.write().unwrap();
+        x.increment(1);
+    }
+
+    pthread.join().unwrap();
+
+    Ok(())
 }
 
 // TODO
@@ -68,3 +117,7 @@ fn main() -> io::Result<()> {
 // while !status {
 //     print duration and state
 // }
+//
+// single or seperated sterr/stdouts?
+//
+// channels or shared reference?
