56a51f1c377c743e95c8636eb8c98c5fbb9f45c7
ozkanonur <work@onurozkan.dev>
2022-08-05 17:16:34 +0300

    save development state
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- main/src/main.rs
+++ main/src/main.rs
@@ -9,13 +9,14 @@ struct ProgressBar<'a> {
     init_message: &'a str,
     final_message: &'a str,
     states: Vec<Arc<Mutex<ProgressState>>>,
-    status: Arc<bool>,
 }
 
+#[derive(Debug)]
 struct ProgressState {
-    state: u8,
-    max_val: u8,
+    state: usize,
+    max_val: usize,
     stdout: Stdout,
+    stderr: Stderr,
 }
 
 impl<'a> ProgressBar<'a> {
@@ -24,50 +25,64 @@ impl<'a> ProgressBar<'a> {
             init_message,
             final_message,
             states: Vec::new(),
-            status: false.into(),
         }
     }
 
-    fn add_bar(&mut self) -> Arc<Mutex<ProgressState>> {
+    fn add_bar(&mut self, max_val: usize) -> Arc<Mutex<ProgressState>> {
         let state = Arc::new(Mutex::new(ProgressState {
             state: 0,
-            max_val: 100,
+            max_val,
             stdout: io::stdout(),
+            stderr: io::stderr(),
         }));
 
         self.states.push(state.clone());
 
         state
     }
+
+    fn did_all_finished(&self) -> bool {
+        for state in self.states.clone() {
+            println!("{:?}", state.lock().unwrap());
+        }
+        true
+    }
 }
 
 impl ProgressState {
-    fn start(&mut self) -> io::Result<()> {
-        let mut handle = self.stdout.lock();
-
-        while self.state <= self.max_val {
-            handle.write_all(format!("\r{}", self.state).as_bytes())?;
-            self.stdout.flush()?;
-
-            thread::sleep(Duration::from_millis(61));
+    fn increment(&mut self, by: usize) {
+        if self.state == self.max_val {
+            return;
         }
 
-        // \n
-        handle.write_all(&[10])?;
+        let mut handle = self.stdout.lock();
+        //        if self.state == 0 {
+        //            // \n
+        //            handle.write_all(&[10]).unwrap();
+        //        }
+        
 
-        Ok(())
-    }
-
-    fn increment(&mut self, by: u8) {
-        if self.state + by > self.max_val {
+        if self.state + by >= self.max_val {
             self.finish();
+            handle
+                .write_all(format!("\r{}\n", self.state).as_bytes())
+                .unwrap();
         } else {
             self.state += by;
+            handle
+                .write_all(format!("\r{}", self.state).as_bytes())
+                .unwrap();
         }
+
+        self.stdout.flush().unwrap();
     }
 
     fn finish(&mut self) {
         self.state = self.max_val;
+    }
+
+    fn is_completed(&mut self) -> bool {
+        self.state == self.max_val
     }
 }
 
@@ -94,21 +109,53 @@ impl ProgressState {
 
 fn main() -> io::Result<()> {
     let mut pbar = ProgressBar::new("Starting", "Finished");
+    let pstate = pbar.add_bar(1000);
 
-    let mut pstate = pbar.add_bar();
+    thread::spawn(move || loop {
+        let mut pstate = pstate.lock().unwrap();
 
-    let pstate_thread = pstate.clone();
-    let pthread = thread::spawn(move || {
-        let mut x = pstate_thread.lock().unwrap();
-        x.start().unwrap();
+        if pstate.is_completed() {
+            break;
+        }
+
+        pstate.increment(12);
+        thread::sleep(Duration::from_millis(61));
+    });
+    let pstate = pbar.add_bar(50000);
+    thread::spawn(move || loop {
+        let mut pstate = pstate.lock().unwrap();
+
+        if pstate.is_completed() {
+            break;
+        }
+
+        pstate.increment(1);
+        thread::sleep(Duration::from_millis(61));
+    });
+    let pstate = pbar.add_bar(150000000);
+    thread::spawn(move || loop {
+        let mut pstate = pstate.lock().unwrap();
+
+        if pstate.is_completed() {
+            break;
+        }
+
+        pstate.increment(5);
+        thread::sleep(Duration::from_millis(61));
+    });
+    let pstate = pbar.add_bar(12222);
+    thread::spawn(move || loop {
+        let mut pstate = pstate.lock().unwrap();
+
+        if pstate.is_completed() {
+            break;
+        }
+
+        pstate.increment(30);
+        thread::sleep(Duration::from_millis(61));
     });
 
-    loop {
-        let mut x = pstate.lock().unwrap();
-        x.increment(1);
-    }
-
-    pthread.join().unwrap();
+    pbar.did_all_finished();
 
     Ok(())
 }
