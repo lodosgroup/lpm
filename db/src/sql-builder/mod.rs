4bd1e7d0c1e1d80b548bd0b8eee55a496177f1c8
Onur Özkan <work@onurozkan.dev>
2022-09-11 13:53:21 +0300

    save dev state
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- db/src/sql-builder/mod.rs
+++ db/src/sql-builder/mod.rs
@@ -1,5 +1,18 @@
-pub enum SqlConstants {
-    // ...
+pub struct SqlBuilder {}
+
+pub enum Operation {
+    // 1st arg: Vector of column names. None means "*".
+    // 2nd arg: Arg for "FROM".
+    Select(Option<Vec<String>>, String),
+    Create(String, Option<CreateOperationArg>),
+    Update(String),
+    Delete(String),
+    Replace(String),
+    Insert(String),
+}
+
+pub enum CreateOperationArg {
+    IfNotExists,
 }
 
 mod select;
