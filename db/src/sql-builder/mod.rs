3f68b1aa5e647461a6468c57e0f7b74077d8ce64
Onur Özkan <work@onurozkan.dev>
2022-09-21 00:29:44 +0300

    save development state
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- db/src/sql-builder/mod.rs
+++ db/src/sql-builder/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub struct SqlBuilder {}
 
 pub enum Operation {
@@ -8,11 +10,62 @@ pub enum Operation {
     Update(String),
     Delete(String),
     Replace(String),
-    Insert(String),
+    Insert(String, Vec<String>),
+}
+
+impl Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Operation::Select(columns, table) => {
+                let columns = match columns {
+                    Some(t) if !t.is_empty() => t.join(","),
+                    _ => String::from("*"),
+                };
+
+                write!(f, "SELECT {} FROM {}", columns, table)
+            }
+            Operation::Create(table, arg) => {
+                if let Some(arg) = arg {
+                    write!(f, "CREATE TABLE {} {}", arg, table)
+                } else {
+                    write!(f, "CREATE TABLE {}", table)
+                }
+            }
+            Operation::Update(table) => {
+                write!(f, "UPDATE {}", table)
+            }
+            Operation::Delete(table) => {
+                write!(f, "DELETE FROM {}", table)
+            }
+            Operation::Replace(_) => todo!(),
+            Operation::Insert(table, columns) => {
+                if columns.is_empty() {
+                    common::log_and_panic!(
+                        "No columns were detected for inserting to table {}",
+                        table
+                    );
+                }
+
+                let columns = columns.join(",");
+
+                // TODO
+                // handle column values
+                write!(f, "INSERT INTO {} ({}) VALUES", table, columns)
+            }
+        }
+    }
 }
 
 pub enum CreateOperationArg {
     IfNotExists,
+}
+
+impl Display for CreateOperationArg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CreateOperationArg::IfNotExists => write!(f, "IF NOT EXISTS"),
+        }
+    }
 }
 
 pub enum ColVal {
