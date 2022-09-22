7a8c046ca42b97fac0aeb79e07b306d41f14f860
Onur Özkan <work@onurozkan.dev>
2022-09-22 14:18:29 +0300

    save dev state
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- db/src/sql-builder/mod.rs
+++ db/src/sql-builder/mod.rs
@@ -1,6 +1,13 @@
 use std::fmt::Display;
 
-pub struct SqlBuilder {}
+pub struct SqlBuilder {
+    pub operation: Operation,
+    pub criteria: Where,
+    // TODO
+    // Order By
+    // Limit
+    // etc
+}
 
 pub enum Operation {
     // 1st arg: Vector of column names. None means "*".
@@ -18,7 +25,7 @@ impl Display for Operation {
         match self {
             Operation::Select(columns, table) => {
                 let columns = match columns {
-                    Some(t) if !t.is_empty() => t.join(","),
+                    Some(t) if !t.is_empty() => t.join(", "),
                     _ => String::from("*"),
                 };
 
@@ -46,11 +53,15 @@ impl Display for Operation {
                     );
                 }
 
+                let prepared_values: Vec<&str> = columns.iter().map(|_| "?").collect();
+                let prepared_values = prepared_values.join(", ");
                 let columns = columns.join(",");
 
-                // TODO
-                // handle column values
-                write!(f, "INSERT INTO {} ({}) VALUES", table, columns)
+                write!(
+                    f,
+                    "INSERT INTO {} ({}) VALUES({})",
+                    table, columns, prepared_values
+                )
             }
         }
     }
@@ -68,25 +79,99 @@ impl Display for CreateOperationArg {
     }
 }
 
-pub enum ColVal {
-    Null,
-    SignedNumber(isize),
-    UnsignedNumber(usize),
-    Text(String),
+pub enum Where {
+    Criteria(WhereOperation),
 }
 
-pub enum Where {
-    Equal(String, ColVal),
-    NotEqual(String, ColVal),
-    LessThan(String, ColVal),
-    LessThanOrEqual(String, ColVal),
-    GreaterThan(String, ColVal),
-    GreaterThanOrEqual(String, ColVal),
-    Between(String, ColVal),
-    In(String, ColVal),
-    Like(String, ColVal),
-    And(Box<Vec<Where>>),
-    Or(Box<Vec<Where>>),
+impl Display for Where {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Where::Criteria(where_ops) => {
+                let condition = match where_ops {
+                    WhereOperation::Equal(index, name) => format!("{} = ?{}", name, index),
+
+                    WhereOperation::NotEqual(index, name) => format!("{} != ?{}", name, index),
+
+                    WhereOperation::LessThan(index, name) => format!("{} < ?{}", name, index),
+
+                    WhereOperation::LessThanOrEqual(index, name) => {
+                        format!("{} <= ?{}", name, index)
+                    }
+
+                    WhereOperation::GreaterThan(index, name) => format!("{} > ?{}", name, index),
+
+                    WhereOperation::GreaterThanOrEqual(index, name) => {
+                        format!("{} >= ?{}", name, index)
+                    }
+
+                    WhereOperation::Between(index1, index2, name) => {
+                        format!("{} BETWEEN ?{} AND ?{}", name, index1, index2)
+                    }
+
+                    WhereOperation::NotBetween(index1, index2, name) => {
+                        format!("{} NOT BETWEEN ?{} AND ?{}", name, index1, index2)
+                    }
+
+                    WhereOperation::In(index, name) => {
+                        format!("{} IN ?{}", name, index)
+                    }
+
+                    WhereOperation::NotIn(index, name) => {
+                        format!("{} NOT IN ?{}", name, index)
+                    }
+
+                    WhereOperation::Like(index, name) => {
+                        format!("{} LIKE ?{}", name, index)
+                    }
+
+                    WhereOperation::NotLike(index, name) => {
+                        format!("{} NOT LIKE ?{}", name, index)
+                    }
+
+                    WhereOperation::And(_) => todo!(),
+                    WhereOperation::Or(_) => todo!(),
+                };
+
+                write!(f, "WHERE {}", condition)
+            }
+        }
+    }
+}
+
+/// Column's index to bind value following with it's name
+pub enum WhereOperation {
+    Equal(u8, String),
+    NotEqual(u8, String),
+    LessThan(u8, String),
+    LessThanOrEqual(u8, String),
+    GreaterThan(u8, String),
+    GreaterThanOrEqual(u8, String),
+    Between(u8, u8, String),
+    NotBetween(u8, u8, String),
+    In(u8, String),
+    NotIn(u8, String),
+    Like(u8, String),
+    NotLike(u8, String),
+    And(Box<Vec<WhereOperation>>),
+    Or(Box<Vec<WhereOperation>>),
 }
 
 mod select;
+
+#[test]
+#[ignore]
+fn custom_test() {
+    let sb = SqlBuilder {
+        operation: Operation::Select(
+            Some(vec![
+                String::from("id"),
+                String::from("name"),
+                String::from("surname"),
+            ]),
+            "users".to_string(),
+        ),
+        criteria: Where::Criteria(WhereOperation::Equal(1, "id".to_string())),
+    };
+    println!("{} {}", sb.operation, sb.criteria);
+    assert!(false);
+}
--- db/src/sql-builder/select.rs
+++ db/src/sql-builder/select.rs
@@ -1,1 +1,15 @@
+use super::*;
 
+impl SqlBuilder {
+    // pub fn select(from: String, columns: Option<Vec<String>>) -> Self {
+    //     Self {
+    //         operation: Operation::Select(columns, from),
+    //     }
+    // }
+
+    // pub fn where_and(from: String, columns: Option<Vec<String>>) -> Self {
+    //     Self {
+    //         operation: Operation::Select(columns, from),
+    //     }
+    // }
+}
