cc3f340758eef8768929524b70a4cd4733a3fd85
ozkanonur <work@onurozkan.dev>
2022-09-29 22:08:36 +0300

    save dev state
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- db/src/sql-builder/select.rs
+++ db/src/sql-builder/select.rs
@@ -1,38 +1,98 @@
 use super::*;
 
-struct Select(String);
+pub enum SelectArg {
+    Distinct(String),
+    Except(Select),
+    Limit(u8),
+    OrderByAsc(String),
+    OrderByDesc(String),
+    Offset(u8),
+    GroupBy(String),
+    Having(Where),
+    InnerJoin(String, String, String),
+    LeftJoin(String, String, String),
+    CrossJoin(String),
+}
+
+impl Display for SelectArg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Distinct(name) => write!(f, "DISTINCT {}", name),
+
+            Self::Except(sql) => write!(f, "EXCEPT {}", sql.0),
+
+            Self::Limit(limit) => write!(f, "LIMIT {}", limit),
+
+            Self::OrderByAsc(name) => write!(f, "ORDER BY {} ASC", name),
+
+            Self::OrderByDesc(name) => write!(f, "ORDER BY {} DESC", name),
+
+            Self::Offset(offset) => write!(f, "OFFSET {}", offset),
+
+            Self::GroupBy(cols) => write!(f, "GROUP BY {}", cols),
+
+            Self::Having(condition) => write!(f, "HAVING {}", condition),
+
+            Self::InnerJoin(table, current_table_column, target_table_column) => write!(
+                f,
+                "INNER JOIN {} ON {} = {}",
+                table, current_table_column, target_table_column
+            ),
+
+            Self::LeftJoin(table, current_table_column, target_table_column) => write!(
+                f,
+                "LEFT JOIN {} ON {} = {}",
+                table, current_table_column, target_table_column
+            ),
+
+            Self::CrossJoin(table) => write!(f, "CROSS JOIN {}", table),
+        }
+    }
+}
+
+pub struct Select(String);
 
 impl Select {
+    #[inline(always)]
     pub fn new(columns: Option<Vec<String>>, from: String) -> Self {
         Self(format!("{}", Operation::Select(columns, from)))
     }
 
-    /// Adds '('
     #[inline(always)]
-    pub fn open_parentheses(&self) -> Self {
+    pub fn add_arg(&self, arg: SelectArg) -> Self {
+        Self(format!("{} {}", self.0, arg))
+    }
+}
+
+impl CommonInstructions for Select {
+    #[inline(always)]
+    fn to_string(&self) -> String {
+        format!("{};", self.0)
+    }
+}
+
+impl WhereInstructions for Select {
+    #[inline(always)]
+    fn open_parentheses(&self) -> Self {
         Self(format!("{} (", self.0))
     }
 
-    /// Adds ')'
     #[inline(always)]
-    pub fn close_parentheses(&self) -> Self {
+    fn close_parentheses(&self) -> Self {
         Self(format!("{} )", self.0))
     }
 
-    /// Only adds 'AND' keyword
     #[inline(always)]
-    pub fn and_keyword(&self) -> Self {
+    fn and_keyword(&self) -> Self {
         Self(format!("{} AND", self.0))
     }
 
-    /// Only adds 'OR' keyword
     #[inline(always)]
-    pub fn or_keyword(&self) -> Self {
+    fn or_keyword(&self) -> Self {
         Self(format!("{} OR", self.0))
     }
 
-    /// Adds contiditon
-    pub fn where_condition(&self, w: Where) -> Self {
+    fn where_condition(&self, w: Where) -> Self {
         if let Some((_, last)) = self.0.rsplit_once(" ") {
             match last {
                 "WHERE" | "(" | "OR" | "AND" => {
@@ -45,53 +105,106 @@ impl Select {
         Self(format!("{} WHERE {}", self.0, w))
     }
 
-    /// Adds contiditon as 'AND'
     #[inline(always)]
-    pub fn and_where(&self, w: Where) -> Self {
+    fn and_where(&self, w: Where) -> Self {
         Self(format!("{} AND {}", self.0, w))
     }
 
-    /// Adds contiditon as 'OR'
     #[inline(always)]
-    pub fn or_where(&self, w: Where) -> Self {
+    fn or_where(&self, w: Where) -> Self {
         Self(format!("{} OR {}", self.0, w))
-    }
-
-    /// Returns prepared statement in String form
-    #[inline(always)]
-    pub fn to_string(&self) -> String {
-        format!("{};", self.0)
     }
 }
 
-#[test]
-#[ignore]
-fn select_builder() {
-    const PRE_ID_ACTIVE: u8 = 0;
-    const PRE_ID_USERNAME: u8 = 1;
-    const PRE_ID_AGE: u8 = 2;
-    const PRE_ID_RETIRED: u8 = 3;
-    const PRE_ID_RETIRED2: u8 = 4;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let select_cols = vec![
-        String::from("id"),
-        String::from("name"),
-        String::from("surname"),
-    ];
+    #[test]
+    fn test_basic_select() {
+        let statement = "SELECT * FROM packages;";
+        let sql = Select::new(None, String::from("packages"));
+        assert_eq!(statement, sql.to_string());
 
-    let sql = Select::new(Some(select_cols), String::from("users"));
+        let statement = "SELECT id, name, kind FROM packages;";
+        let cols = vec![
+            String::from("id"),
+            String::from("name"),
+            String::from("kind"),
+        ];
+        let sql = Select::new(Some(cols), String::from("packages"));
+        assert_eq!(statement, sql.to_string());
+    }
 
-    let sql = sql
-        .where_condition(Where::NotEqual(PRE_ID_ACTIVE, String::from("active")))
-        .or_keyword()
-        .open_parentheses()
-        .where_condition(Where::Equal(PRE_ID_USERNAME, String::from("username")))
-        .and_where(Where::GreaterThanOrEqual(PRE_ID_AGE, String::from("age")))
-        .and_where(Where::NotEqual(PRE_ID_RETIRED, String::from("retired")))
-        .close_parentheses()
-        .or_where(Where::NotEqual(PRE_ID_RETIRED2, String::from("retired")));
+    #[test]
+    fn test_select_with_conditions() {
+        let expected = "SELECT * FROM people WHERE name = ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::Equal(1, String::from("name")));
+        assert_eq!(expected, sql.to_string());
 
-    println!("{}", sql.to_string());
+        let expected = "SELECT * FROM people WHERE name != ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::NotEqual(1, String::from("name")));
+        assert_eq!(expected, sql.to_string());
 
-    assert!(false);
+        let expected = "SELECT * FROM people WHERE age < ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::LessThan(1, String::from("age")));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE age <= ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::LessThanOrEqual(1, String::from("age")));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE age > ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::GreaterThan(1, String::from("age")));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE age >= ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::GreaterThanOrEqual(1, String::from("age")));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE age BETWEEN ?1 AND ?2;";
+        let sql = Select::new(None, String::from("people")).where_condition(Where::Between(
+            1,
+            2,
+            String::from("age"),
+        ));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE age NOT BETWEEN ?1 AND ?2;";
+        let sql = Select::new(None, String::from("people")).where_condition(Where::NotBetween(
+            1,
+            2,
+            String::from("age"),
+        ));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE age IN ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::In(1, String::from("age")));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE age NOT IN ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::NotIn(1, String::from("age")));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE name LIKE ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::Like(1, String::from("name")));
+        assert_eq!(expected, sql.to_string());
+
+        let expected = "SELECT * FROM people WHERE name NOT LIKE ?1;";
+        let sql = Select::new(None, String::from("people"))
+            .where_condition(Where::NotLike(1, String::from("name")));
+        assert_eq!(expected, sql.to_string());
+    }
+
+    #[test]
+    fn test_select_with_nested_conditions() {}
 }
