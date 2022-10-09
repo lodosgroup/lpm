9358c36ab925ac98625f0dce171a70d520f0e019
ozkanonur <work@onurozkan.dev>
2022-10-09 20:33:23 +0300

    update o.s rule of building
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- main/build.rs
+++ main/build.rs
@@ -1,8 +1,14 @@
-use std::path::Path;
-
+#[cfg(target_family = "unix")]
 fn main() {
+    use std::path::Path;
+
     let home_path: &'static str = env!("HOME");
     let target_dir = Path::new(&home_path).join(".local/share/min_sqlite3_sys");
 
     println!("cargo:rustc-link-arg=-Wl,-rpath={}", target_dir.display());
+}
+
+#[cfg(not(target_family = "unix"))]
+fn main() {
+    common::log_and_panic!("Lpm can only build on unix systems");
 }
