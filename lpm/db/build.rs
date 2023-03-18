8f4239b56701d876c2187680e16a400d8b13a366
ozkanonur <work@onurozkan.dev>
2023-03-18 19:43:24 +0300

    replace lzma with lz4
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- lpm/db/build.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-
-fn main() {
-    let home_path: &'static str = env!("HOME");
-    let target_dir = Path::new(&home_path).join(".local/share/min_sqlite3_sys");
-
-    println!("cargo:rustc-link-arg=-Wl,-rpath={}", target_dir.display());
-}
