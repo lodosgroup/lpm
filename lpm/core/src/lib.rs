4a0d4e72cab1c389599a73cec9e7583f3b552b2a
ozkanonur <work@onurozkan.dev>
2023-03-26 00:19:04 +0300

    implement plugin handling via FFI
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- lpm/core/src/lib.rs
+++ lpm/core/src/lib.rs
@@ -3,10 +3,12 @@ const EXTRACTION_OUTPUT_PATH: &str = "/var/cache/lpm";
 mod delete;
 mod extract;
 mod install;
+mod plugin;
 mod update;
 mod validate;
 
 pub use delete::PkgDeleteTasks;
 pub use extract::PkgExtractTasks;
 pub use install::PkgInstallTasks;
+pub use plugin::PluginController;
 pub use update::PkgUpdateTasks;
