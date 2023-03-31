817a747daaafabcb39c6ed91c31a23786a4cc7f3
ozkanonur <work@onurozkan.dev>
2023-03-31 18:44:52 +0300

    link `tiny_lz4_decoder_sys` and `min_sqlite3_sys` on sdk
    Signed-off-by: ozkanonur <work@onurozkan.dev>

---
+++ sdk/build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    use std::path::Path;
+
+    let home_path: &'static str = env!("HOME");
+    let sqlite_so = Path::new(&home_path).join(".local/share/min_sqlite3_sys");
+    let lz4_so = Path::new(&home_path).join(".local/share/tiny_lz4_decoder_sys");
+
+    println!(
+        "cargo:rustc-link-arg=-Wl,-rpath={},-rpath={}",
+        sqlite_so.display(),
+        lz4_so.display()
+    );
+}
