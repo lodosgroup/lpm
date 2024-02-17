c180e389f1c68b32519a8ce4d7a83c8ab0c5ed9c
onur-ozkan <work@onurozkan.dev>
2024-02-18 01:02:18 +0300

    add compatible args in build script
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- sdk/build.rs
+++ sdk/build.rs
@@ -5,9 +5,6 @@ fn main() {
     let sqlite_so = Path::new(&home_path).join(".local/share/min_sqlite3_sys");
     let lz4_so = Path::new(&home_path).join(".local/share/tiny_lz4_decoder_sys");
 
-    println!(
-        "cargo:rustc-link-arg=-Wl,-rpath={},-rpath={}",
-        sqlite_so.display(),
-        lz4_so.display()
-    );
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", sqlite_so.display());
+    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lz4_so.display());
 }
